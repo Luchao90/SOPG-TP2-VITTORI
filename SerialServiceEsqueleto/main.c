9d1015738172597b2cc5eb9a2da6f15306c84637
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-16 20:23:04 -0300

    educiaa-read

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -7,7 +7,7 @@
 #define msToUs (x)
 
 #define BUFFER_LENGTH 20
-#define MILLISECONDS_500 (500 * 1000U)
+#define MILLISECONDS_100 (100 * 1000U)
 
 typedef struct
 {
@@ -20,21 +20,25 @@ typedef struct
 int main(void)
 {
 	SerialPort mainPort = {.file = 0, .buffer = ">OUTS:2,2,2,2\r\n", .number = 1, .baudrate = 115200};
-	int bytes_length;
+	char echo_terminal[BUFFER_LENGTH];
+	int bytes_length = 0;
 
 	printf("Inicio Serial Service\r\n");
 	mainPort.file = serial_open(mainPort.number, mainPort.baudrate);
+	serial_send(mainPort.buffer, strlen(mainPort.buffer));
 
 	while (1)
 	{
-		/* bytes_length = serial_receive(mainPort.buffer, strlen(mainPort.buffer));
-		usleep(MILLISECONDS_500); */
-		strcpy(mainPort.buffer, ">OUTS:1,0,1,0\r\n");
-		serial_send(mainPort.buffer, strlen(mainPort.buffer));
-		sleep(1);
-		strcpy(mainPort.buffer, ">OUTS:0,1,0,1\r\n");
-		serial_send(mainPort.buffer, strlen(mainPort.buffer));
-		sleep(1);
+		bytes_length = serial_receive(mainPort.buffer, BUFFER_LENGTH);
+		usleep(MILLISECONDS_100);
+
+		strncpy(echo_terminal, mainPort.buffer, bytes_length);
+		echo_terminal[bytes_length] = '\0';
+
+		if (bytes_length > 0)
+		{
+			printf("RX: %s", echo_terminal);
+		}
 	}
 
 	exit(EXIT_SUCCESS);
