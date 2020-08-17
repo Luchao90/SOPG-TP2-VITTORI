fb50a9842d90cad349e9805ab823150e90c93665
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-17 19:01:43 -0300

    thread created

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -21,28 +21,48 @@ Terminal_t console = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.echo[0] = END_STRING};
 
+SerialPort mainPort = {
+	.mutex = PTHREAD_MUTEX_INITIALIZER,
+	.file = 0,
+	.buffer = ">OUTS:2,2,2,2\r\n",
+	.number = 1,
+	.baudrate = 115200,
+	.bytesReaded = 0};
+
 pthread_t usb, tcp;
 void *thread_usb(void *nothing);
+void *thread_tcp(void *nothing);
 
 int main(void)
 {
 	void *usb_ret;
 	int thread_usb_created;
+	int thread_tcp_created;
 
 	signals_init();
 	signals_thread_disable();
 	thread_usb_created = pthread_create(&usb, NULL, thread_usb, NULL);
+	thread_tcp_created = pthread_create(&usb, NULL, thread_tcp, NULL);
 	if (thread_usb_created != 0)
 	{
 		errno = thread_usb_created;
 		perror("pthread_create");
 		return -1;
 	}
+	else if (thread_tcp_created != 0)
+	{
+		errno = thread_tcp_created;
+		perror("pthread_create");
+		return -1;
+	}
 	else
 	{
 		signals_thread_enable();
-		printf("Running..\r\n");
-		pthread_join(usb, &usb_ret);
+		pthread_mutex_lock(&console.mutex);
+		printf("thread main\r\n");
+		pthread_mutex_unlock(&console.mutex);
+		pthread_join(usb, NULL);
+		pthread_join(tcp, NULL);
 		printf("\r\nExit succesful\r\n");
 
 		exit(EXIT_SUCCESS);
@@ -52,14 +72,10 @@ int main(void)
 
 void *thread_usb(void *nothing)
 {
-	SerialPort mainPort = {
-		.file = 0,
-		.buffer = ">OUTS:2,2,2,2\r\n",
-		.number = 1,
-		.baudrate = 115200,
-		.bytesReaded = 0};
-
+	pthread_mutex_lock(&console.mutex);
 	printf("thread USB\r\n");
+	pthread_mutex_unlock(&console.mutex);
+
 	mainPort.file = serial_open(mainPort.number, mainPort.baudrate);
 	serial_send(mainPort.buffer, strlen(mainPort.buffer));
 
@@ -74,6 +90,27 @@ void *thread_usb(void *nothing)
 		if (mainPort.bytesReaded > 0)
 		{
 			printf("RX: %s", console.echo);
+		}
+		pthread_mutex_unlock(&console.mutex);
+	}
+	return NULL;
+}
+
+void *thread_tcp(void *nothing)
+{
+	pthread_mutex_lock(&console.mutex);
+	printf("thread TCP\r\n");
+	pthread_mutex_unlock(&console.mutex);
+
+	while (1)
+	{
+		sleep(1);
+		pthread_mutex_lock(&console.mutex);
+		strncpy(console.echo, mainPort.buffer, mainPort.bytesReaded);
+		console.echo[mainPort.bytesReaded] = END_STRING;
+		if (mainPort.bytesReaded > 0)
+		{
+			printf("TCP: %s", console.echo);
 		}
 		pthread_mutex_unlock(&console.mutex);
 	}
