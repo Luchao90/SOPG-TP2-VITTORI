5727fe4e0a8bc566ff2c786099f95da3907abeed
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-17 19:54:19 -0300

    listening

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -7,9 +7,16 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #define msToUs(x) (x * 1000U)
 #define END_STRING '\0'
+#define PORT 10000
+#define SA struct sockaddr
 
 typedef struct
 {
@@ -20,6 +27,8 @@ typedef struct
 Terminal_t console = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.echo[0] = END_STRING};
+
+void console_print(const char *string);
 
 SerialPort mainPort = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
@@ -81,6 +90,7 @@ void *thread_usb(void *nothing)
 
 	while (1)
 	{
+		/* Send blinky option to edu ciaa */
 		mainPort.bytesReaded = serial_receive(mainPort.buffer, BUFFER_LENGTH);
 		usleep(msToUs(50));
 
@@ -98,21 +108,106 @@ void *thread_usb(void *nothing)
 
 void *thread_tcp(void *nothing)
 {
-	pthread_mutex_lock(&console.mutex);
-	printf("thread TCP\r\n");
-	pthread_mutex_unlock(&console.mutex);
+	int sockfd, connfd, len;
+	struct sockaddr_in serveraddr, clientaddr;
+	char buff[BUFFER_LENGTH];
+	int n;
 
+	console_print("thread TCP\r\n");
+
+	/* Create socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		console_print("socket creation failed...\n");
+		exit(0);
+	}
+	else
+	{
+		console_print("Socket successfully created..\n");
+	}
+	/* assign IP, PORT  */
+	bzero(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(PORT);
+	//serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	if (inet_pton(AF_INET, "127.0.0.1", &(serveraddr.sin_addr)) <= 0)
+	{
+		console_print("ERROR invalid server IP\r\n");
+		return NULL;
+	}
+
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (SA *)&serveraddr, sizeof(serveraddr))) != 0)
+	{
+		console_print("socket bind failed...\n");
+		exit(0);
+	}
+	else
+	{
+		console_print("Socket successfully binded..\n");
+	}
+
+	// Now server is ready to listen and verification
+	if ((listen(sockfd, 5)) != 0)
+	{
+		console_print("Listen failed...\n");
+		exit(0);
+	}
+	else
+	{
+		console_print("Server listening..\n");
+	}
+
+	len = sizeof(clientaddr);
+	// Accept the data packet from client and verification
+	connfd = accept(sockfd, (SA *)&clientaddr, &len);
+	if (connfd < 0)
+	{
+		console_print("server acccept failed...\n");
+		exit(0);
+	}
+	else
+	{
+		console_print("server acccept the client...\n");
+	}
+
+	// infinite loop for chat
 	while (1)
 	{
-		sleep(1);
+		bzero(buff, BUFFER_LENGTH);
+
+		// read the message from client and copy it in buffer
+		read(sockfd, buff, sizeof(buff));
+		// print buffer which contains the client contents
 		pthread_mutex_lock(&console.mutex);
-		strncpy(console.echo, mainPort.buffer, mainPort.bytesReaded);
-		console.echo[mainPort.bytesReaded] = END_STRING;
-		if (mainPort.bytesReaded > 0)
-		{
-			printf("TCP: %s", console.echo);
-		}
+		printf("From client: %s\t To client : ", buff);
 		pthread_mutex_unlock(&console.mutex);
+		bzero(buff, BUFFER_LENGTH);
+		n = 0;
+		// copy server message in the buffer
+		while ((buff[n++] = getchar()) != '\n')
+			;
+
+		// and send that buffer to client
+		/* write(sockfd, buff, sizeof(buff)); */
+
+		// if msg contains "Exit" then server exit and chat ended.
+		if (strncmp("exit", buff, 4) == 0)
+		{
+			console_print("Server Exit...\n");
+			break;
+		}
 	}
+
+	// After chatting close the socket
+	close(sockfd);
 	return NULL;
+}
+
+void console_print(const char *string)
+{
+	pthread_mutex_lock(&console.mutex);
+	printf("%s", string);
+	pthread_mutex_unlock(&console.mutex);
 }
