12bc7778be70b92b7f7c0fa5fc5c4b22fce9cf67
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-18 14:35:35 -0300

    refactoring

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -15,14 +15,17 @@
 
 #define msToUs(x) (x * 1000U)
 #define END_STRING '\0'
+#define USB_MIN_PAYLOAD sizeof(">OK")
 #define PORT 10000
 #define SA struct sockaddr
 #define CONNECTION_LOST -1
+#define LISTEN_QUEUE_LENGTH 5
 #define PAYLOAD_LENGTH sizeof(":STATESXYWZ") //:STATESXYWZ
 #define LINE_A_STATE_POSITION 7
 #define LINE_B_STATE_POSITION 8
 #define LINE_C_STATE_POSITION 9
 #define LINE_D_STATE_POSITION 10
+#define LINE_X_CHANGE_POSITION 14
 
 typedef struct
 {
@@ -49,13 +52,13 @@ typedef struct
 	char ipClient[32];
 } Socket_t;
 
-Socket_t mysocket = {
+Socket_t tcp = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.connection = CONNECTION_LOST};
 
 void console_print(const char *string);
 
-SerialPort mainPort = {
+SerialPort usb = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.file = 0,
 	.buffer = ">OUTS:2,2,2,2\r\n",
@@ -63,7 +66,7 @@ SerialPort mainPort = {
 	.baudrate = 115200,
 	.bytesReaded = 0};
 
-pthread_t usb, tcp;
+pthread_t usb_thread, tcp_thread;
 void *thread_usb(void *nothing);
 void *thread_tcp(void *nothing);
 
@@ -74,8 +77,8 @@ int main(void)
 
 	signals_init();
 	signals_thread_disable();
-	thread_usb_created = pthread_create(&usb, NULL, thread_usb, NULL);
-	thread_tcp_created = pthread_create(&tcp, NULL, thread_tcp, NULL);
+	thread_usb_created = pthread_create(&usb_thread, NULL, thread_usb, NULL);
+	thread_tcp_created = pthread_create(&tcp_thread, NULL, thread_tcp, NULL);
 
 	if (thread_usb_created != 0)
 	{
@@ -97,13 +100,13 @@ int main(void)
 		{
 			sleep(1);
 		}
-		pthread_cancel(usb);
-		pthread_cancel(tcp);
-		pthread_join(usb, NULL);
-		pthread_join(tcp, NULL);
+		pthread_cancel(usb_thread);
+		pthread_cancel(tcp_thread);
+		pthread_join(usb_thread, NULL);
+		pthread_join(tcp_thread, NULL);
 		serial_close();
-		close(mysocket.sockfd);
-		close(mysocket.newfd);
+		close(tcp.sockfd);
+		close(tcp.newfd);
 		printf("\r\nExit program\r\n");
 
 		exit(EXIT_SUCCESS);
@@ -115,44 +118,41 @@ void *thread_usb(void *nothing)
 {
 	console_print("thread USB\r\n");
 
-	pthread_mutex_lock(&mainPort.mutex);
-	mainPort.file = serial_open(mainPort.number, mainPort.baudrate);
-	pthread_mutex_unlock(&mainPort.mutex);
+	pthread_mutex_lock(&usb.mutex);
+	usb.file = serial_open(usb.number, usb.baudrate);
+	pthread_mutex_unlock(&usb.mutex);
 
 	while (1)
 	{
-		pthread_mutex_lock(&mainPort.mutex);
-		mainPort.bytesReaded = serial_receive(mainPort.buffer, BUFFER_LENGTH);
-		strncpy(console.echo, mainPort.buffer, mainPort.bytesReaded);
-		console.echo[mainPort.bytesReaded] = END_STRING;
-		pthread_mutex_unlock(&mainPort.mutex);
+		pthread_mutex_lock(&usb.mutex);
+		usb.bytesReaded = serial_receive(usb.buffer, BUFFER_LENGTH);
+		strncpy(console.echo, usb.buffer, usb.bytesReaded);
+		console.echo[usb.bytesReaded] = END_STRING;
+		pthread_mutex_unlock(&usb.mutex);
 		usleep(msToUs(50));
 
-		if (mainPort.bytesReaded > 0)
+		if (usb.bytesReaded >= USB_MIN_PAYLOAD)
 		{
 			pthread_mutex_lock(&console.mutex);
-			printf("EDU-CIAA: %s", console.echo);
+			printf("USB-RX: %s", console.echo);
 			pthread_mutex_unlock(&console.mutex);
 
-			if (strncmp(mainPort.buffer, ">OK\r\n", strlen(">OK\r\n")) != 0)
+			if (strncmp(usb.buffer, ">OK\r\n", strlen(">OK\r\n")) != 0)
 			{
-				//Creacion del buffer para enviar por el socket
-				pthread_mutex_lock(&mysocket.mutex);
-				sprintf(mysocket.buff_tx, ":LINE%cTG\n", mainPort.buffer[14]);
-				pthread_mutex_unlock(&mysocket.mutex);
+				/* Socket tx buffer create */
+				pthread_mutex_lock(&tcp.mutex);
+				sprintf(tcp.buff_tx, ":LINE%cTG\n", usb.buffer[LINE_X_CHANGE_POSITION]);
+				pthread_mutex_unlock(&tcp.mutex);
 
+				/* console echo */
 				pthread_mutex_lock(&console.mutex);
-				printf("Envio por el socket: %s", mysocket.buff_tx);
+				printf("Send to socket: %s", tcp.buff_tx);
 				pthread_mutex_unlock(&console.mutex);
 
-				if (mysocket.connection != CONNECTION_LOST)
+				//Escribo por el socket
+				if (write(tcp.newfd, tcp.buff_tx, sizeof(tcp.buff_tx)) == -1)
 				{
-					//Escribo por el socket
-					if (write(mysocket.newfd, mysocket.buff_tx, strlen(mysocket.buff_tx)) == -1)
-					{
-						perror("Error escribiendo mensaje en el socket\r\n");
-						exit(1);
-					}
+					console_print("No socket connection, waiting for new\r\n");
 				}
 			}
 		}
@@ -167,8 +167,8 @@ void *thread_tcp(void *nothing)
 	console_print("thread TCP\r\n");
 
 	/* Create socket */
-	mysocket.sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (mysocket.sockfd == -1)
+	tcp.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (tcp.sockfd == -1)
 	{
 		console_print("Socket creation failed...\r\n");
 		pthread_exit(NULL);
@@ -177,26 +177,26 @@ void *thread_tcp(void *nothing)
 	{
 		console_print("Socket successfully created..\r\n");
 	}
-	/* assign IP, PORT  */
-	bzero(&mysocket.serveraddr, sizeof(mysocket.serveraddr));
-	mysocket.serveraddr.sin_family = AF_INET;
-	mysocket.serveraddr.sin_port = htons(PORT);
-	//mysocket.serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	if (inet_pton(AF_INET, "127.0.0.1", &(mysocket.serveraddr.sin_addr)) <= 0)
+	/* assign IP and PORT  */
+	bzero(&tcp.serveraddr, sizeof(tcp.serveraddr));
+	tcp.serveraddr.sin_family = AF_INET;
+	tcp.serveraddr.sin_port = htons(PORT);
+	//tcp.serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	if (inet_pton(AF_INET, "127.0.0.1", &(tcp.serveraddr.sin_addr)) <= 0)
 	{
 		console_print("ERROR invalid server IP\r\n");
 		pthread_exit(NULL);
 	}
 
-	// Binding newly created socket to given IP and verification
-	if ((bind(mysocket.sockfd, (SA *)&mysocket.serveraddr, sizeof(mysocket.serveraddr))) != 0)
+	/* Binding newly created socket to given IP and verification */
+	if ((bind(tcp.sockfd, (SA *)&tcp.serveraddr, sizeof(tcp.serveraddr))) != 0)
 	{
 		console_print("Socket bind failed...\r\n");
 		pthread_exit(NULL);
 	}
 
-	// Now server is ready to listen and verification
-	if ((listen(mysocket.sockfd, 10)) != 0)
+	/* Now server is ready to listen and verification */
+	if ((listen(tcp.sockfd, LISTEN_QUEUE_LENGTH)) != 0)
 	{
 		console_print("Listen failed...\r\n");
 		pthread_exit(NULL);
@@ -208,51 +208,56 @@ void *thread_tcp(void *nothing)
 
 	while (1)
 	{
-		mysocket.len = sizeof(mysocket.clientaddr);
-		// Accept the data packet from client and verification
-		mysocket.newfd = accept(mysocket.sockfd, (SA *)&mysocket.clientaddr, &mysocket.len);
-		if (mysocket.newfd < 0)
+		tcp.len = sizeof(tcp.clientaddr);
+
+		/* Accept the data packet from client and verification */
+		tcp.newfd = accept(tcp.sockfd, (SA *)&tcp.clientaddr, &tcp.len);
+		if (tcp.newfd < 0)
 		{
 			console_print("Server acccept failed...\r\n");
 			pthread_exit(NULL);
 		}
 
-		inet_ntop(AF_INET, &(mysocket.clientaddr.sin_addr), mysocket.ipClient, sizeof(mysocket.ipClient));
+		inet_ntop(AF_INET, &(tcp.clientaddr.sin_addr), tcp.ipClient, sizeof(tcp.ipClient));
 		pthread_mutex_lock(&console.mutex);
-		printf("Server connection from:  %s\r\n", mysocket.ipClient);
+		printf("Server connection from:  %s\r\n", tcp.ipClient);
 		pthread_mutex_unlock(&console.mutex);
-		mysocket.connection = 1;
+		tcp.connection = 1;
 
-		while (mysocket.connection != CONNECTION_LOST)
+		while (tcp.connection != CONNECTION_LOST)
 		{
-			if ((mysocket.n = read(mysocket.newfd, mysocket.buff, sizeof(mysocket.buff))) == -1)
+			if ((tcp.n = read(tcp.newfd, tcp.buff, sizeof(tcp.buff))) == -1)
 			{
 				console_print("Error leyendo mensaje en socket\r\n");
 				pthread_exit(NULL);
 			}
-			if (mysocket.n == 0)
+			if (tcp.n == 0)
 			{
-				mysocket.connection = CONNECTION_LOST;
-				close(mysocket.newfd);
+				tcp.connection = CONNECTION_LOST;
+				close(tcp.newfd);
 				console_print("Connection lost, waiting for new\r\n");
 			}
 
-			if (mysocket.n >= PAYLOAD_LENGTH)
+			if (tcp.n >= PAYLOAD_LENGTH)
 			{
-				mysocket.buff[mysocket.n] = END_STRING;
-				line_A_state = mysocket.buff[LINE_A_STATE_POSITION];
-				line_B_state = mysocket.buff[LINE_B_STATE_POSITION];
-				line_C_state = mysocket.buff[LINE_C_STATE_POSITION];
-				line_D_state = mysocket.buff[LINE_D_STATE_POSITION];
+				tcp.buff[tcp.n] = END_STRING;
+				line_A_state = tcp.buff[LINE_A_STATE_POSITION];
+				line_B_state = tcp.buff[LINE_B_STATE_POSITION];
+				line_C_state = tcp.buff[LINE_C_STATE_POSITION];
+				line_D_state = tcp.buff[LINE_D_STATE_POSITION];
 
 				pthread_mutex_lock(&console.mutex);
-				printf("Interace service: %s", mysocket.buff);
+				printf("Interace service: %s", tcp.buff);
 				pthread_mutex_unlock(&console.mutex);
 
-				pthread_mutex_lock(&mainPort.mutex);
-				sprintf(mainPort.buffer, ">OUTS:%c,%c,%c,%c\r\n", line_A_state, line_B_state, line_C_state, line_D_state);
-				serial_send(mainPort.buffer, sizeof(mainPort.buffer));
-				pthread_mutex_unlock(&mainPort.mutex);
+				pthread_mutex_lock(&usb.mutex);
+				sprintf(usb.buffer, ">OUTS:%c,%c,%c,%c\r\n", line_A_state, line_B_state, line_C_state, line_D_state);
+				serial_send(usb.buffer, sizeof(usb.buffer));
+				pthread_mutex_unlock(&usb.mutex);
+
+			    pthread_mutex_lock(&console.mutex);
+				printf("USB-TX: %s", usb.buffer);
+				pthread_mutex_unlock(&console.mutex);
 			}
 		}
 		sleep(1);
